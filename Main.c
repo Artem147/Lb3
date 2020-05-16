18392070c058bcc40376c2267a4bc79aacb2466c
Your NameArtem147 <shirkunovartem@gmail.com>
2020-05-16 21:20:32 +0300

    Создание мейна, формирование исходного массива.

--- Main.c
+++ Main.c
@@ -1,5 +1,23 @@
-#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
 
-main(){
-	printf("Privet");
+int main(){
+	int K;//Длина разбиения.
+	int n;//Количество элементов.
+	int i;//Счетчики.
+	printf("Split length: ");
+	scanf("%d",&K);
+	printf("Number of elements: ");
+	scanf("%d ", &n);
+	if (n < K) return 0;
+	int* A;//Исходный массив.
+	A = (int*)malloc(n * sizeof(int));
+	for (i = 0; i < n; i++)//Ввод исходного массива.
+		scanf("%d", &A[i]);
+	for (i = 0; i < n; i++)//Вывод исходного массива.
+		printf("%d ", A[i]);
+	system("pause");
+	return 0;
 }
