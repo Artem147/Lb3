0f1bd6335d1e56b40c3fae635b0a832839866e5d
Your NameArtem147 <shirkunovartem@gmail.com>
2020-05-18 22:34:36 +0300

    Добавлены комментарии

--- Source.c
+++ Source.c
@@ -8,35 +8,50 @@ int* InputData(int n, int N, int* A)
 {
 	int i;
 	A = malloc(n * sizeof(int));
-	if(n<N) return A;//Return для массива ans[].
+	//Return для массива ans[].
+	if(n<N) return A;
 	for(i=0; i<n; i++)
 		scanf("%d", &A[i]);
-	return A;//Return для исходного массива.
+	//Return для исходного массива.
+	return A;
 }
+//Ставит новый элемент в дек в порядке неубывания.
 int Comparison(Deque* D) 
-{
+{//Пока новый элемент меньше минимума или начало меньше конца.
 	while (D->data[D->final]< D->data[(D->final)-1] && D->final != D->start)
-	{
+	{//Меньший элемент встает на место большего.
 		D->data[(D->final) - 1] = D->data[D->final];
 		D->final--;
 	}
 }
-int CleanDeque(Deque* D, int* A, int K) 
-{
-	int i;
+//Выводит минимальный элемент и удаляет его из дека, 
+//если он равен первому элементу подотрезка
+int CleanDeque(Deque* D, int* A, int K, int i) 
+{//Если дек пуст
 	if (isEmpty) return 0;
 	if (D->data[D->start] == A[i - K])
-		PopFront(D);
+		return PopFront(D);
+	else return D->data[D->start];
 }
+//Добавляет элемент в дек. Если подотрезок пройден, выводит минимум в массив ans. 
 int Function(Deque* D, int K, int* A, int n)
 {
-	int i;
+	int i,j=0;
+	//Массив с минимумами на подотрезках.
 	int* ans=NULL;
+	//Выделение памяти массива с ответом.
 	ans = InputData(n - K + 1, n, A);
 	for (i = 0; i < n; i++)
 	{
-		if (i % K) CleanDeque(D,A,K);
-		PushBack(D,A[i]);//Добавляем новый элемент в конец дека.
+		//Если пройден подотрезок, записываем минимум в ответ. 
+		if (i % K)
+		{
+			ans[j] = CleanDeque(D, A, K, i);
+			j++;
+		}
+		//Добавляем новый элемент в конец дека.
+		PushBack(D,A[i]);
+		//Сравнение элементов, минимум оказывается на вершине дека.
 		Comparison(D);
 	}
 }
